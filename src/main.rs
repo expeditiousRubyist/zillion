615cfc40ffa1b39c854689a44ce12a0e218fac78
rubyist <rubyist.personal@openmailbox.org>
2021-03-01 18:34:04 -0800

    Rewrite to use updated googology crate, and use Clap for command line arguments

--- src/main.rs
+++ src/main.rs
@@ -14,56 +14,82 @@
  * limitations under the License.
 */
 
+#[macro_use]
+extern crate clap;
 extern crate googology;
 
-use std::env;
 use std::io;
+use googology::ParseError;
+use googology::conway_wechsler::Scale;
 
-use googology::conway_wechsler::{full_name, power_of_ten};
-
-fn usage() {
-	println!("Usage: zillion [OPTIONS] [num]");
-	println!("Produces a Conway-Wechsler name for a given number");
-	println!("Running without arguments causes num to be read from stdin");
-	println!("");
-	println!("Options:");
-	println!("    -h, --help     Print this message");
-	println!("    -p, --power    Compute for 10^num instead");
+fn handle_knuth(digits: &str, pow: bool, _: Scale) -> Result<String, ParseError> {
+	use googology::knuth_yllion::{full_name, power_of_ten};
+	if pow { power_of_ten(digits) }
+	else { full_name(digits) }
 }
 
-fn handle_arguments(argv : Vec<String>) {
-	match argv[0].as_str() {
-		"-h" | "--help" => usage(),
-		"-p" | "--power" => 
-			if argv.len() > 1 {
-				println!("{}", power_of_ten(&argv[1], true).unwrap());
-			}
-			else { panic!("No argument provided for --power"); },
-		num  => println!("{}", full_name(num, true).unwrap())
-	}
+fn handle_conway(digits: &str, pow: bool, s: Scale) -> Result<String, ParseError> {
+	use googology::conway_wechsler::{full_name, power_of_ten};
+	if pow { power_of_ten(digits, s) }
+	else { full_name(digits, s) }
 }
 
-fn handle_stdin() {
+fn handle_stdin(
+	power: bool,
+	scale: Scale,
+	namefn: fn(&str, bool, Scale) -> Result<String, ParseError>
+) -> Result<(), ParseError> {
 	let input = io::stdin();
 	let mut buf = String::new();
 
 	while let Ok(_) = input.read_line(&mut buf) {
 		{
 			let stripped = buf.trim();
-			if stripped.is_empty() {
-				break;
-			}
-			println!("{}", full_name(stripped, true).unwrap());
+			if stripped.is_empty() { break; }
+
+			let output = namefn(stripped, power, scale)?;
+			println!("{}", output);
 		}
 		buf.clear();
 	}
+
+	Ok(())
 }
 
-fn main() {
-	let argv : Vec<String> = env::args().skip(1).collect();
-	match argv.len() {
-		0 => handle_stdin(),
-		_ => handle_arguments(argv),
+fn main() -> Result<(), ParseError> {
+	// Parse command line arguments
+	let matches = clap_app!(zillion =>
+		(version: "0.5.0")
+		(author: "Rubyist")
+		(about: "Produces a natural language name for a given number")
+		(@arg POWER: -p --power "Compute a name for a power of ten")
+		(@arg NAMEFN: -n --namefn +takes_value possible_value[conway knuth]
+			"Scheme for transforming numbers into names (default = conway)")
+		(@arg SCALE: -s --scale +takes_value possible_value[short british peletier]
+			"Scale used when Conway-Wechsler naming scheme is used (default = short)")
+		(@arg INPUT: "Input number (Uses STDIN if not present)")
+	).get_matches();
+
+	let power  = matches.is_present("POWER");
+	let namefn = matches.value_of("NAMEFN").map(|s| match s {
+		"conway" => handle_conway,
+		"knuth"  => handle_knuth,
+		_        => unreachable!(),
+	}).unwrap_or(handle_conway);
+	
+	let scale  = matches.value_of("SCALE").map(|s| match s {
+		"short"    => Scale::Short,
+		"british"  => Scale::LongBritish,
+		"peletier" => Scale::LongPeletier,
+		_          => unreachable!(),
+	}).unwrap_or(Scale::Short);
+	
+	if let Some(input) = matches.value_of("INPUT") {
+		let output = namefn(input, power, scale)?;
+		println!("{}", output);
+		Ok(())
+	}
+	else {
+		handle_stdin(power, scale, namefn)
 	}
 }
-
